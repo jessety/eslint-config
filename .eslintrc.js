250bdecc7ebd92ec40ed4aa6a10bdfa5ad026311
Jesse Youngblood <jessetyoungblood@gmail.com>
2023-04-23 11:58:49 -0400

    Adjust rules:
    - Better detect test files
    - Enable `pathGroupsExcludedImportTypes` in `import/order`

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,17 +11,26 @@ module.exports = {
       extends: ['./javascript.js'],
     },
     {
-      files: ['**/*.{test,spec}.ts'],
+      files: [
+        '**/*.{test,spec}.ts',
+        '**/__fixtures__/**/*.ts',
+        '**/__tests__/**/*.ts',
+      ],
       extends: ['./base.js'],
       env: { jest: true },
       rules: {
         'no-console': 'warn',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-empty-function ': 'off',
       },
     },
     {
-      files: ['**/*.{test,spec}.{js,cjs,mjs}'],
+      files: [
+        '**/*.{test,spec}.{js,cjs,mjs}',
+        '**/__fixtures__/**/*.{js,cjs,mjs}',
+        '**/__tests__/**/*.{js,cjs,mjs}',
+      ],
       extends: ['./javascript.js'],
       env: { jest: true },
       rules: {
